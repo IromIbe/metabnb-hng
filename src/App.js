d5fa7e799e42d9b6b2f7f74bae47638bbbfacc2c
IromIbe <iromibe@outlook.com>
2022-11-10 22:31:56 +0100

    chore: set up metabnb locally with create react app and tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Homepage from "./components/homepage/homepage";
+import ConnectWallet from "./components/connect_wallet/connect_wallet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/connect_wallet' element={<ConnectWallet />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
